84311008c9a7af279d4af46e7c5b7e54c5ca803b
hoijoii <oodh3328@gmail.com>
2022-10-31 17:39:14 +0900

    design home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="home">
+  <div id="home">
     <img src="@/assets/images/bg-earth.jpg" class="bg-earth">
+    <div class=container>    
+        <div class="home-txt main">
+          <div class="semi-title">SO, YOU WANT TO TRAVEL TO</div>
+          <div class="title space">SPACE</div>
+          <p class="p-txt">Let’s face it; if you want to go to space, 
+            you might as well genuinely go to outer space 
+            and not hover kind of on the edge of it. 
+            Well sit back, and relax because we’ll give you 
+            a truly out of this world experience!</p>
+        </div>
+        <router-link class="circle" to="/destination">
+          <button class="circle-btn">
+            <div class="txt font-bel">EXPLORE</div>
+          </button>
+        </router-link>
+    </div>
 
-    <div></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+//import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   components: {
+  },
+  setup() {
+    /* let hover = ref(0)
+
+    const doMouseOver = () => {
+      hover=1;
+      console.log(hover)
+    }
+
+    const doMouseLeave = () => {
+      hover=0;
+      console.log(hover)
+    }
+
+    return {
+      doMouseOver, doMouseLeave, hover
+    } */
   }
 }
 </script>
