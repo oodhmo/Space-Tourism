051b8779f1a00739e63594043716276d497436c4
hoijoii <oodh3328@gmail.com>
2022-11-01 17:49:12 +0900

    Add crew page

--- src/stores/commons.js
+++ src/stores/commons.js
@@ -1,7 +1,11 @@
 import { defineStore } from 'pinia'
 
 export const useCommonsStore = defineStore('commonsStore', {
-  state: () => ({ menuActive: "00", tabActive: "0" }),
+  state: () => ({
+    menuActive: "00",
+    tabActive: "0",
+    pageActive: "0"
+  }),
   actions: {
     setMenuActive(menuActive) {
       this.menuActive = menuActive || "00"
@@ -11,6 +15,13 @@ export const useCommonsStore = defineStore('commonsStore', {
       this.tabActive = tabActive || "0"
 
       console.log(this.tabActive)
+    },
+
+    setPageActive(pageActive) {
+      this.pageActive = pageActive || "0"
+
+      console.log(this.pageActive)
     }
   }
-})
+}
+)
--- src/views/CrewView.vue
+++ src/views/CrewView.vue
@@ -1,11 +1,67 @@
 <template>
   <div id="crew">
     <img src="@/assets/images/bg-crew.png" class="bg-default">
+
+    <div class="semi-title">
+      <strong class="count">02</strong>
+      MEET YOUR CREW
+    </div>
+
+    <div class="info">
+      <div class="crewmates">
+
+        <div class="type">{{crewInfo.section.type}}</div>
+        <div class="title">{{crewInfo.section.name}}</div>
+        <div class="main p-txt description">{{crewInfo.section.description}}</div>
+
+        <div class="page">
+          <div :class="['circle', { 'clicked' : commonsStore.pageActive === '0' }]" @click="() => {
+            commonsStore.setPageActive('0')
+            setPageActive() }"/>
+          <div :class="['circle', { 'clicked' : commonsStore.pageActive === '1' }]" @click="() => {
+            commonsStore.setPageActive('1')
+            setPageActive() }" />
+          <div :class="['circle', { 'clicked' : commonsStore.pageActive === '2' }]" @click="() => {
+            commonsStore.setPageActive('2')
+            setPageActive() }" />
+          <div :class="['circle', { 'clicked' : commonsStore.pageActive === '3' }]" @click="() => {
+            commonsStore.setPageActive('3')
+            setPageActive() }" />
+        </div>
+
+      </div>
+      <div class="pic">
+        <img :src="require(`@/assets/images/${crewInfo.section.imgUrl}`)" class="img">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import { useCommonsStore } from '@/stores/commons'
+
 export default {
+  setup() {
+    const crew = require('@/assets/data/character-info.json').CREW
+    let crewInfo = reactive([])
+
+    const commonsStore = useCommonsStore()
+
+    const setPageActive = () => {
+      crew.forEach((mate)=>{
+        if (commonsStore.pageActive === mate.id) {
+          crewInfo.section = mate
+        }
+      })
+    }
+
+    return { crew, crewInfo, setPageActive, commonsStore }
+  },
+
+  created() {
+    this.setPageActive()
+  }
 
 }
 </script>
