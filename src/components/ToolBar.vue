59360f7241d85ec9e883be832bdce8083e0aa459
hoijoii <oodh3328@gmail.com>
2022-11-08 15:37:42 +0900

    Add sidebar animation

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="toolbar">
-    <div class="bar">
-      <div class="menu">
-        <div :class="['items', {'bor-b' : tab.to === this.$route.path, 'bor-hov' : tab.to !== this.$route.path}]" v-for="tab in tabs" :key="tab.tabId">
-          <router-link :to="tab.to">
-            <div class="item" @click="commonsStore.resetMenuActive()">
-              <strong class="itemId">{{tab.tabId}}</strong> 
-              {{tab.title}}
-            </div>
-          </router-link>
+    <div class="btn">
+        <img v-if="!isClicked" src="@/assets/images/menu.png" class="menu-btn" @click="setClicked">
+        <img v-if="isClicked" src="@/assets/images/menu_x.png" class="menu-btn" @click="resetClicked">
+    </div>
+    <nav :class="['', {'open': isClicked}]">
+      <div class="bar">  
+        <div class="menu">
+          <div :class="['items', {'bor-b' : tab.to === this.$route.path, 'bor-hov' : tab.to !== this.$route.path}]" v-for="tab in tabs" :key="tab.tabId">
+            <router-link :to="tab.to">
+              <div class="item" @click="commonsStore.resetMenuActive()">
+                <strong class="itemId">{{tab.tabId}}</strong> 
+                {{tab.title}}
+              </div>
+            </router-link>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
     <img src="@/assets/images/bar-line.png" class="line">
-    <img src="@/assets/images/bar-icon.png" class="icon">
+      <img src="@/assets/images/bar-icon.png" class="icon">
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useCommonsStore } from '@/stores/commons'
 
 export default defineComponent({
@@ -26,9 +32,22 @@ export default defineComponent({
     const tabs = require('@/assets/data/tabs-info.json').TOOLBAR.items
     const commonsStore = useCommonsStore()
 
+    let isClicked = ref(false)
+
+    const setClicked = () => {
+      isClicked.value = true
+    }
+
+    const resetClicked = () => {
+      isClicked.value = false
+    }
+
     return {
       tabs,
       commonsStore,
+      isClicked, 
+      setClicked,
+      resetClicked
     }
   },
 })
